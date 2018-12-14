0b8fd519241bbcfae0a02d2ede47601142d1590c
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-14 21:21:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -203,30 +203,6 @@ client.on('message', function(msg) {
 });
 
 client.on('message', message => {
-    var prefix = "*;
-    if(message.author.bot) return;
-    var name1 = message.mentions.users.first();
-    var reason = message.content.split(' ').slice(2).join(' ');
-    if(message.content.startsWith(prefix + 'report')) {
-        if(message.author.bot) return;
-        if(!message.guild.channels.find('name', 'report')) return message.channel.send('**نرجو عمل روم باسم report**').then(msg => msg.delete(5000));
-    if(!name1) return message.reply('**:innocent:منشن:innocent:**').then(msg => msg.delete(3000))
-        message.delete();
-    if(!reason) return message.reply('**:innocent:وش سوى؟:innocent:**').then(msg => msg.delete(3000))
-        message.delete();
-    var abod = new Discord.RichEmbed()
-    .setTitle(`بلاغ من قبل: ${message.author.tag}`)
-    .addField('**المجرم:**', `${name1}`, true)
-    .addField('**بروم:**', `${message.channel.name}`, true)
-    .addField('**البلاغ:**', `${reason}`, true)
-    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL)
-    .setTimestamp()
-        message.guild.channels.find('name', 'report').sendEmbed(abod)
-    message.reply('**:sunglasses:بنأخذ حقك:sunglasses:**').then(msg => msg.delete(3000));
-    }
-});
-
-client.on('message', message => {
 var prefix = "*;
        if(message.content === prefix + "cl") {
                            if(!message.channel.guild) return message.reply('** This command only for servers**');
