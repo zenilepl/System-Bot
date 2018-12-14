eebc9c3d1869f20f4f28578023499abdac301336
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-14 21:20:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
     var prefix = "*"
